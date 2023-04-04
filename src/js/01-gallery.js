2060b03c7ef6de93f3febab38f353a16c2f14113
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-04 14:54:15 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -16,61 +16,18 @@ const galleryMarkup=createGalleryMarkup(galleryItems);
 
 containerGallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-containerGallery.addEventListener('click', onGalleryItemClick);
-
-let instance;
+let gallery = new SimpleLightbox('.gallery a', {captionDelay: 250, showCounter: false});
 
 function createGalleryMarkup(gallery){
  return gallery.map(({preview, original, description})=>{
     return `
     <li class="gallery__item">
-      <a class="gallery__link" href="${original}" download>
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+        </a>
     </li>
     `;
   })
   .join("");
 }
 
-function onGalleryItemClick (event){
-    event.preventDefault();
-
-    const isImageGalleryEl=event.target.classList.contains('gallery__image');
-    if(!isImageGalleryEl){
-        return;
-    }
-    
-    openGalleryItemInModal(event.target.dataset.source);
-}
-
-function openGalleryItemInModal(src){
-  instance = basicLightbox.create(`
-    <img src="${src}" width="800" height="600">
-`,{
-  onShow: (instance) => {
-    window.addEventListener('keydown', onEscKeyPress)
-  },
-  onClose: (instance) => {
-    window.removeEventListener('keydown', onEscKeyPress)}
-  });
-
-  instance.show();
- // window.addEventListener('keydown', onEscKeyPress);
-  
-}
-
-function onEscKeyPress(event) {
-  const ESC_KEY_CODE = 'Escape';
-  const isEscKey = event.code === ESC_KEY_CODE;
-  
-  if (isEscKey) {
-    //window.removeEventListener('keydown', onEscKeyPress);
-    instance.close();
-  }
-}
