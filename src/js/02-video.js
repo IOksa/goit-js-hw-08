1d8ebc9a0d40d84475fe50dda302b61f0a286154
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-04 17:24:43 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+console.log("iframe=", iframe);
+const player = new Vimeo.Player(iframe);
+console.log("player=", player);
+const objPlayer={
+    'videoplayer-current-time': 0,
+};
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    objPlayer['videoplayer-current-time']=data;
+    console.log("objPlayer['videoplayer-current-time']=", objPlayer['videoplayer-current-time']);
+};
+
+player.on('timeupdate', onPlay);
+
+
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
