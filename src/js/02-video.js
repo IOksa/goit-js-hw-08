3c0ef899a5eb1ea1797e8dfd5f384b9df7fca66c
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-09 18:24:17 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,17 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-console.log("iframe=", iframe);
-const player = new Vimeo.Player(iframe);
-console.log("player=", player);
-const objPlayer={
-    'videoplayer-current-time': 0,
-};
 
-player.on('play', function() {
-    console.log('played the video!');
-});
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-
+const player = new Player(iframe);
 
 const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-    objPlayer['videoplayer-current-time']=data;
-    console.log("objPlayer['videoplayer-current-time']=", objPlayer['videoplayer-current-time']);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 };
+player.on('timeupdate', throttle(onPlay,1000));
 
-player.on('timeupdate', onPlay);
-
-
-
-player.setCurrentTime(30.456).then(function(seconds) {
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)).seconds).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs={
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+populateTextarea();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+function onFormSubmit(evt) {
+    evt.preventDefault();
+  
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    console.log("email: ", formData.email);
+    console.log("message: ", formData.message);
+}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ */
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY,  JSON.stringify(formData));
+}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+      formData.email=JSON.parse(savedMessage).email;
+      formData.message=JSON.parse(savedMessage).message;
+      refs.input.value=formData.email;
+      refs.textarea.value = formData.message;
+    }
+}
